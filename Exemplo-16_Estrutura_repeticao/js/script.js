5cc0d3c78301beeac11eddbaa71c1fd958092261
Klynger Queiroz <klynger.queiroz@grupovat.com.br>
2023-03-07 16:15:03 -0400

    estudando estruturas de atribuicao

--- Exemplo-16_Estrutura_repeticao/js/script.js
+++ Exemplo-16_Estrutura_repeticao/js/script.js
@@ -6,3 +6,18 @@ while(x < 3){
     console.log('O nome desse carro e ' + arr[x], 'ele esta na posicao ' + x);
     x++;  
 }
+
+//Estrutura For //
+
+for(let i = 0; i < 5; i++){
+    console.log(i)
+}
+
+//teste//
+let arr3 = ['aviao','motocicleta','barco', 'carro','bicileta'];
+
+for(let i = 0; i < arr3.length; i++){
+    console.log(arr3[i]);
+}
+
+console.log(arr.length)
---
+++ Exemplo-17_OP_Atribuicao/js/script.js
@@ -0,0 +1,14 @@
+var x = 1;
+var y = 2;
+
+//soma
+console.log(x += y);
+
+//subtracao
+console.log(x -= y);
+
+//multiplicacao
+console.log(x *= y);
+
+//divisao
+console.log(x /=y);
