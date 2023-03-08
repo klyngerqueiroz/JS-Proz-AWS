b3378a35fceb85dce734bc81e9d51593af22a375
Klynger Queiroz <klynger.queiroz@grupovat.com.br>
2023-03-08 11:31:55 -0400

    estudando break e continue

---
+++ Exemplo-18_Break_Continue/js/script.js
@@ -0,0 +1,21 @@
+for(var i = 0; i < 10; i--){
+    console.log(i);
+    if(i === 5);
+    break;
+}
+console.log('Parou de contar');
+
+
+
+let x = 10;
+
+while(x < 100){
+    x += 10;
+    if(x === 40 || x === 70 || x === 80){
+        console.log('continue')
+    continue
+    }
+   
+    console.log('testado o continue ' + x);
+   
+}
