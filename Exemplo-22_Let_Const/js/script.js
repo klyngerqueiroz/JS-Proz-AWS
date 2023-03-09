f542a3699024a87823333edb49420fe43f6d5d39
Klynger Queiroz <klynger.queiroz@grupovat.com.br>
2023-03-09 13:37:49 -0400

    estudando Let e Const

---
+++ Exemplo-22_Let_Const/js/script.js
@@ -0,0 +1,28 @@
+let x = 5;
+
+x = 202;
+x == x;
+
+const y = 20;
+
+
+console.log('const global ' + y);
+
+console.log(typeof x);
+console.log('saida sera ' + x)
+
+if(true){
+
+    let x = 20
+    console.log(x);
+
+    const y = 5;
+    console.log('const if ' + y);
+}
+
+console.log(x);
+
+
+for(let j = 0; j<10; j++){
+    console.log('contagem ' + j);
+}
